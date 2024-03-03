e4aa02a17b73e274928631a33f57c0b3238bab8d
saliltrehan <161879484+saliltrehan@users.noreply.github.com>
2024-03-03 11:49:52 +0000

    config_entity + artifact_entity +data_ingestion + constants= Done

--- xray/components/data_ingestion.py
+++ xray/components/data_ingestion.py
@@ -4,7 +4,7 @@ import inspect
 from zipfile import ZipFile
 from xray.logger import logging
 from xray.exception import CustomException
-from xray.configuration.gcloud_syncer import GCloudSync
+from xray.configuration.cloud_storage import S3Operation
 from xray.entity.config_entity import DataIngestionConfig
 from xray.entity.artifact_entity import DataIngestionArtifacts
 
@@ -12,21 +12,20 @@ from xray.entity.artifact_entity import DataIngestionArtifacts
 class DataIngestion:
     def __init__(self, data_ingestion_config: DataIngestionConfig):
         self.data_ingestion_config = data_ingestion_config
-        self.gcloud = GCloudSync()
+        self.s3 = S3Operation()
         
-    def get_data_from_cloud(self) -> None:
+    def get_data_from_s3(self) -> None:
         current_function_name = inspect.stack()[0][3]
+        logging.info(f"Entered the {current_function_name} method of {self.__class__.__name__} class")        
         try:
-            logging.info(f"Entered the {current_function_name} method of {self.__class__.__name__} class")
-            os.makedirs(self.data_ingestion_config.DATA_INGESTION_ARTIFACTS_DIR, exist_ok=True)
-            
-            self.gcloud.sync_folder_from_gcloud(
-                self.data_ingestion_config.BUCKET_NAME,
-                self.data_ingestion_config.ZIP_FILE_NAME,
-                self.data_ingestion_config.DATA_INGESTION_ARTIFACTS_DIR,
+            self.s3.sync_folder_from_s3(
+                folder=self.data_ingestion_config.data_path,
+                bucket_name=self.data_ingestion_config.bucket_name,
+                bucket_folder_name=self.data_ingestion_config.s3_data_folder,
             )
 
             logging.info(f"Exited the {current_function_name} method of {self.__class__.__name__} class")
+
         except Exception as e:
             raise CustomException(e, sys) from e
 
@@ -42,23 +41,22 @@ class DataIngestion:
             return self.data_ingestion_config.DATA_ARTIFACTS_DIR, self.data_ingestion_config.NEW_DATA_ARTIFACTS_DIR
         except Exception as e:
             raise CustomException(e, sys) from e
-    
+
     def initiate_data_ingestion(self) -> DataIngestionArtifacts:
         current_function_name = inspect.stack()[0][3]
         logging.info(f"Entered the {current_function_name} method of {self.__class__.__name__} class")
-        
+
         try:
-            self.get_data_from_cloud()
-            logging.info(f"Fetched the data from GCloud Bucket using {current_function_name} method of {self.__class__.__name__} class")
-            
-            imbalance_data_file_path, raw_data_file_path = self.unzip_and_clean()
-            logging.info(f"Unzipped file & split into train and validation set using {current_function_name} method of {self.__class__.__name__} class")
-                        
-            data_ingestion_artifacts = DataIngestionArtifacts(
-                imbalance_data_file_path = imbalance_data_file_path, 
-                raw_data_file_path = raw_data_file_path
+            self.get_data_from_s3()
+
+            data_ingestion_artifact: DataIngestionArtifacts = DataIngestionArtifacts(
+                train_file_path=self.data_ingestion_config.train_data_path,
+                test_file_path=self.data_ingestion_config.test_data_path,
             )
-            
-            return data_ingestion_artifacts
+
+            logging.info(f"Exited the {current_function_name} method of {self.__class__.__name__} class")
+
+            return data_ingestion_artifact
+
         except Exception as e:
-            raise CustomException(e, sys) from e        
+            raise CustomException(e, sys) from e
