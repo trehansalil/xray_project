11ef01a5a285612d2395c8d5a719d4850e18a14a
saliltrehan <161879484+saliltrehan@users.noreply.github.com>
2024-03-04 09:12:05 +0000

    model evaluation

--- xray/components/model_evaluation.py
+++ xray/components/model_evaluation.py
@@ -1,164 +1,135 @@
-import os
-import pickle
+import inspect
 import sys
-import inspect
-import pandas as pd
-import numpy as np
-import inspect
-from xray.logger import logging
-from xray.exception import CustomException
+from typing import Tuple
+import torch
+from torch.nn import CrossEntropyLoss, Module
+from torch.optim import SGD, Optimizer
+from torch.utils.data import DataLoader
+
+from xray.entity.artifact_entity import (
+    DataTransformationArtifact,
+    ModelEvaluationArtifact,
+    ModelTrainerArtifact,
+)
 from xray.entity.config_entity import ModelEvaluationConfig
-from xray.entity.artifact_entity import ModelTrainerArtifact, ModelEvaluationArtifact, DataTransformationArtifact
-from keras.preprocessing.text import Tokenizer
-import tensorflow as tf
-from keras.utils import pad_sequences
-from xray.ml.model import ModelArchitecture
-from xray.constants import *
-from xray.configuration.gcloud_syncer import GCloudSync
-from sklearn.metrics import confusion_matrix
+from xray.exception import CustomException
+from xray.logger import logging
+from xray.ml.model import Net
 
 class ModelEvaluation:
-    def __init__(self, 
-                 model_evaluation_config: ModelEvaluationConfig, 
-                 model_trainer_artifacts: ModelTrainerArtifact,
-                 data_transformation_artifacts: DataTransformationArtifact):
-        
-        """
-        :param model_evaluation_config: Configuration for model evaluation            
-        model = model_architecture.get_model()
-        data transformation artifact stage
-        :param model_trainer_artifacts: Output reference of model trainer artifact stage
-        """
+    def __init__(
+        self,
+        data_transformation_artifact: DataTransformationArtifact,
+        model_evaluation_config: ModelEvaluationConfig,
+        model_trainer_artifact: ModelTrainerArtifact,
+    ):
 
+        self.data_transformation_artifact = data_transformation_artifact
         self.model_evaluation_config = model_evaluation_config
-        self.model_trainer_artifacts = model_trainer_artifacts
-        self.data_transformation_artifacts = data_transformation_artifacts
-        self.gcloud = GCloudSync()
 
+        self.model_trainer_artifact = model_trainer_artifact
 
-    
-    def get_best_model_from_gcloud(self) -> str:
-        """
-        :return: Fetch best model from gcloud storage and store inside best model directory path
-        """
-        current_function_name = inspect.stack()[0][3]
-        logging.info(f"Entered the {current_function_name} method of {self.__class__.__name__} class")        
-        try:
-            os.makedirs(self.model_evaluation_config.BEST_MODEL_DIR_PATH, exist_ok=True)
-
-            self.gcloud.sync_folder_from_gcloud(self.model_evaluation_config.BUCKET_NAME,
-                                                self.model_evaluation_config.MODEL_NAME,
-                                                self.model_evaluation_config.BEST_MODEL_DIR_PATH)
-
-            best_model_path = os.path.join(self.model_evaluation_config.BEST_MODEL_DIR_PATH,
-                                           self.model_evaluation_config.MODEL_NAME)
-            logging.info(f"Exited the {current_function_name} method of {self.__class__.__name__} class")
-            
-            return best_model_path
-        
-        except Exception as e:
-            raise CustomException(e, sys) from e 
-        
-
-    
-    def evaluate(self):
-        """
-
-        :param model: Currently trained model or best model from gcloud storage
-        :param data_loader: Data loader for validation dataset
-        :return: loss
-        """
-        current_function_name = inspect.stack()[0][3]
-        logging.info(f"Entered the {current_function_name} method of {self.__class__.__name__} class")         
-        try:
-            print(self.model_trainer_artifacts.x_test_path)
-
-            x_test = pd.read_csv(self.model_trainer_artifacts.x_test_path,index_col=0)
-            print(x_test)
-            y_test = pd.read_csv(self.model_trainer_artifacts.y_test_path,index_col=0)
-
-            with open('tokenizer.pickle', 'rb') as handle:
-                tokenizer = pickle.load(handle)
-
-            load_model=tf.keras.models.load_model(self.model_trainer_artifacts.trained_model_path)
-
-            x_test = x_test['tweet'].astype(str)
-
-            x_test = x_test.squeeze()
-            y_test = y_test.squeeze()
-
-            test_sequences = tokenizer.texts_to_sequences(x_test)
-            test_sequences_matrix = pad_sequences(test_sequences,maxlen=MAX_LEN)
-            print(f"----------{test_sequences_matrix}------------------")
-
-            print(f"-----------------{x_test.shape}--------------")
-            print(f"-----------------{y_test.shape}--------------")
-            accuracy = load_model.evaluate(test_sequences_matrix,y_test)
-            logging.info(f"the test accuracy is {accuracy}")
-
-            lstm_prediction = load_model.predict(test_sequences_matrix)
-            res = []
-            for prediction in lstm_prediction:
-                if prediction[0] < 0.5:
-                    res.append(0)
-                else:
-                    res.append(1)
-            print(confusion_matrix(y_test,res))
-            logging.info(f"the confusion_matrix is {confusion_matrix(y_test,res)} ")
-            logging.info(f"Exited the {current_function_name} method of {self.__class__.__name__} class")
-            
-            return accuracy
-        except Exception as e:
-            raise CustomException(e, sys) from e
-        
-
-    
-    def initiate_model_evaluation(self) -> ModelEvaluationArtifact:
-        """
-            Method Name :   initiate_model_evaluation
-            Description :   This function is used to initiate all steps of the model evaluation
-
-            Output      :   Returns model evaluation artifact
-            On Failure  :   Write an exception log and then raise an exception
-        """
+    def configuration(self) -> Tuple[DataLoader, Module, float, Optimizer]:
         current_function_name = inspect.stack()[0][3]
         logging.info(f"Entered the {current_function_name} method of {self.__class__.__name__} class") 
+
         try:
+            test_dataloader: DataLoader = (
+                self.data_transformation_artifact.transformed_test_object
+            )
 
-            logging.info("Loading currently trained model")
-            trained_model=tf.keras.models.load_model(self.model_trainer_artifacts.trained_model_path)
-            with open('tokenizer.pickle', 'rb') as handle:
-                load_tokenizer = pickle.load(handle)
+            model: Module = Net()
 
-            trained_model_accuracy = self.evaluate()
+            model: Module = torch.load(self.model_trainer_artifact.trained_model_path)
 
-            logging.info("Fetch best model from gcloud storage")
-            best_model_path = self.get_best_model_from_gcloud()
+            model.to(self.model_evaluation_config.DEVICE)
 
-            logging.info("Check is best model present in the gcloud storage or not ?")
-            if os.path.isfile(best_model_path) is False:
-                is_model_accepted = True
-                logging.info("glcoud storage model is false and currently trained model accepted is true")
+            cost: Module = CrossEntropyLoss()
 
-            else:
-                logging.info("Load best model fetched from gcloud storage")
-                best_model=tf.keras.models.load_model(best_model_path)
-                best_model_accuracy= self.evaluate()
+            '''optimizer: Optimizer = SGD(
+                model.parameters(), **self.model_evaluation_config.OPTIMIZER_PARAMS
+            )'''
 
-                logging.info("Comparing loss between best_model_loss and trained_model_loss ? ")
-                if best_model_accuracy > trained_model_accuracy:
-                    is_model_accepted = True
-                    logging.info("Trained model not accepted")
-                else:
-                    is_model_accepted = False
-                    logging.info("Trained model accepted")
+            model.eval()
 
-            model_evaluation_artifacts = ModelEvaluationArtifact(is_model_accepted=is_model_accepted)
-            
+            logging.info(f"Exited the {current_function_name} method of {self.__class__.__name__} class") 
+
+            return test_dataloader, model, cost
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
+    def test_net(self) -> float:
+        current_function_name = inspect.stack()[0][3]
+        logging.info(f"Entered the {current_function_name} method of {self.__class__.__name__} class") 
+
+        try:
+            test_dataloader, net, cost = self.configuration()
+
+            with torch.no_grad():
+                holder = []
+
+                for _, data in enumerate(test_dataloader):
+                    images = data[0].to(self.model_evaluation_config.DEVICE)
+
+                    labels = data[1].to(self.model_evaluation_config.DEVICE)
+
+                    output = net(images)
+
+                    loss = cost(output, labels)
+
+                    predictions = torch.argmax(output, 1)
+
+                    for i in zip(images, labels, predictions):
+                        h = list(i)
+
+                        holder.append(h)
+
+                    logging.info(
+                        f"Actual_Labels : {labels}     Predictions : {predictions}     labels : {loss.item():.4f}"
+                    )
+
+                    self.model_evaluation_config.TEST_LOSS += loss.item()
+
+                    self.model_evaluation_config.TEST_ACCURACY += (
+                        (predictions == labels).sum().item()
+                    )
+
+                    self.model_evaluation_config.TOTAL_BATCH += 1
+
+                    self.model_evaluation_config.TOTAL += labels.size(0)
+
+                    logging.info(
+                        f"Model  -->   Loss : {self.model_evaluation_config.TEST_LOSS/ self.model_evaluation_config.TOTAL_BATCH} Accuracy : {(self.model_evaluation_config.TEST_ACCURACY / self.model_evaluation_config.TOTAL) * 100} %"
+                    )
+
+            accuracy = (
+                self.model_evaluation_config.TEST_ACCURACY
+                / self.model_evaluation_config.TOTAL
+            ) * 100
+
+            logging.info(f"Exited the {current_function_name} method of {self.__class__.__name__} class") 
+
+            return accuracy
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
+    def initiate_model_evaluation(self) -> ModelEvaluationArtifact:
+        current_function_name = inspect.stack()[0][3]
+        logging.info(f"Entered the {current_function_name} method of {self.__class__.__name__} class") 
+
+        try:
+            accuracy = self.test_net()
+
+            model_evaluation_artifact: ModelEvaluationArtifact = (
+                ModelEvaluationArtifact(model_accuracy=accuracy)
+            )
+
             logging.info("Returning the ModelEvaluationArtifacts")
-            logging.info(f"Exited the {current_function_name} method of {self.__class__.__name__} class")       
-                 
-            return model_evaluation_artifacts
+            logging.info(f"Exited the {current_function_name} method of {self.__class__.__name__} class")     
+
+            return model_evaluation_artifact
 
         except Exception as e:
             raise CustomException(e, sys) from e
